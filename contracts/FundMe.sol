5335068545b5c6eb5c300df783c51a22325d99af
prosper74 <atu.prosper@gmail.com>
2025-08-19 15:35:39 +0100

    Created the FUndMe contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUSD = 5e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUSD, "Did not send enough eth");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;        
+    }
+}
