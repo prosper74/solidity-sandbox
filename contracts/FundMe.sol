41872c46cf34c12656618ec18f222afabe784b85
prosper74 <atu.prosper@gmail.com>
2025-08-22 13:20:07 +0100

    modified the code to save gas

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,20 +3,22 @@ pragma solidity ^0.8.29;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUSD = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUSD, "Did not send enough eth");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Did not send enough eth");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;        
     }
@@ -42,7 +44,10 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can withdraw");
+        // require(msg.sender == i_owner, "Only owner can withdraw");
+        if(msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
 }
